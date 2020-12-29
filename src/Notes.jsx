09427affcf427be07b556871f52c2ae9685b0c3c
prashant1682 <prashant009@gmail.com>
2020-12-29 11:20:50 +0530

    update

---
+++ src/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Today</h1>
+      <p>Complete React.</p>
+    </div>
+  );
+}
+
+export default Notes;
